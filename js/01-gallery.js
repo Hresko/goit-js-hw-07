362b0bdbe3458490edc7348630d5edc9fa074545
Hreskoo <hresko21@gmail.com>
2022-08-29 01:49:39 +0300

    01-galery(bagfix)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,49 +1,53 @@
 import { galleryItems } from './gallery-items.js';
-console.log(galleryItems);
 // Change code below this line
+console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
-const markupGalary = galleryItems.reduce(
-  (acc, { preview, original, description }) =>
-    acc +
-    `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </div>`,
-  ''
-);
-gallery.innerHTML = markupGalary;
+const createMarkupGalary = galleryArrey =>
+    galleryArrey
+        .map(
+            ({ preview, original, description }) =>
+                `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+                </div>`
+        )
+        .join('');
 
-gallery.addEventListener('click', onImageClick);
+gallery.innerHTML = createMarkupGalary(galleryItems);
 
-function onImageClick(e) {
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-  e.preventDefault();
-  onOpenModal(e);
+let modalWindow = null;
+
+const showLightboxModal = ({ alt, dataset: { source } }) => {
+    modalWindow = basicLightbox.create(`<img style="color: #fff" src="${source}" alt="${alt}" width="800" height="600">`, {
+        onShow: addKeyboardControls,
+        onClose: removeKeyboardControls,
+    });
+
+    modalWindow.show();
+};
+
+const addKeyboardControls = () => window.addEventListener('keydown', onWindowKeyDown);
+const removeKeyboardControls = () => window.removeEventListener('keydown', onWindowKeyDown);
+
+const onWindowKeyDown = ({ code }) => {
+    if (code != 'Escape') return;
+
+    modalWindow.close();
 }
 
-function onOpenModal(e) {
-  const instance = basicLightbox.create(`
-    <img src="${e.target.dataset.source}" width="1280" height="960">
-`);
-  instance.show();
-  addEventListener('keydown', onEscapePress);
+const onGalleryContainerClick = event => {
+    event.preventDefault();
 
-  function onEscapePress(e) {
-    const ESC_KEY_CODE = 'Escape';
-    const isEscape = e.code === ESC_KEY_CODE;
+    if (event.target.nodeName !== 'IMG') return;
+    showLightboxModal(event.target);
+};
 
-    if (isEscape) {
-      instance.close();
-    }
-  }
-}
+gallery.addEventListener('click', onGalleryContainerClick);
